25e269e96f76667e9e5a6741c3c7ae5fcbfc04e6
Aditya Patil <aditya.patil@mavenir.com>
2023-05-07 16:59:45 +0530

    init

---
+++ index.js
@@ -0,0 +1,57 @@
+const WebSocketServer = require("ws").Server;
+const express = require("express");
+const path = require("path");
+const server = require("http").createServer();
+
+const app = express();
+
+app.use(express.json());
+const wss = new WebSocketServer({ server: server });
+wss.on("connection", (ws, req) => {
+  console.log(`Connection request from: ${req.connection.remoteAddress}`);
+  ws.on("message", (data) => {
+    console.log("data: " + data);
+    const json = JSON.parse(data);
+    const request = json.request;
+    const message = json.message;
+    const channel = json.channel;
+    console.log(request, message, channel, channels);
+    switch (request) {
+      case "JOIN_LOBBY":
+        joinLobby(ws, channel);
+        publishMessage({ message, channel }, channel);
+        break;
+      case "MESSAGE":
+        publishMessage({ message, channel }, channel);
+        break;
+    }
+  });
+  ws.on("close", () => {
+    console.log("Stopping client connection.");
+  });
+});
+
+server.on("request", app);
+server.listen(8080, () => {
+  console.log("Server listening on http://localhost:8080");
+});
+
+var channels = {};
+
+function createLobby(ws, channel) {
+  channels[channel] = [ws];
+}
+
+function publishMessage(message, channel) {
+  channels[channel].forEach((sub) => {
+    sub.send(
+      JSON.stringify({
+        message,
+      })
+    );
+  });
+}
+
+function joinLobby(ws, channel) {
+  channels[channel].push(ws);
+}
